95ee1c1dc178080c23bd9d4d67f0f70630f1eeac
decanus <7621705+decanus@users.noreply.github.com>
2020-07-26 23:35:12 +0200

    changes

--- main.go
+++ main.go
@@ -209,7 +209,7 @@ func GeneratePin() string {
 	return string(b)
 }
 
-var table = [...]byte{'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'}
+var table = []byte{'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'}
 
 func getType(t string) (error, webrtc.SDPType) {
 	switch t {

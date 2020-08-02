a21ad24272901a8b944a3c07ee1c2e67cdb1648c
decanus <7621705+decanus@users.noreply.github.com>
2020-08-02 21:11:31 +0200

    fix

--- db/tables.sql
+++ db/tables.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS users (
   id SERIAL PRIMARY KEY,
   display_name VARCHAR(256) NOT NULL,
   username VARCHAR(100) NOT NULL,
-  email VARCHAR(254) NOT NULL,
+  email VARCHAR(254) NOT NULL
 );
 
 CREATE INDEX idx_email ON users (email);

4d04f3be9fcf9366b4d509cd06da65d2daf05960
decanus <7621705+decanus@users.noreply.github.com>
2020-08-03 01:46:40 +0200

    started

--- vagrant/bin/boot.sh
+++ vagrant/bin/boot.sh
@@ -5,5 +5,5 @@ PATH=$PATH:/usr/local/bin
 setsebool httpd_can_network_connect on -P
 
 sudo service nginx start
-
-go run $GOPATH/src/github.com/ephemeral-networks/voicely/main.go &
+sudo systemctl start supervisord
+sudo systemctl enable supervisord
--- vagrant/bin/provision.sh
+++ vagrant/bin/provision.sh
@@ -13,6 +13,13 @@ sudo yum install -y nginx
 
 sudo yum install -y golang
 
+sudo yum install -y supervisor
+
+rm -rf /etc/supervisord.conf
+sudo ln -s /vagrant/conf/supervisord.conf /etc/supervisord.conf
+sudo mkdir -p /etc/supervisor/conf.d/
+sudo ln -s /vagrant/conf/voicely.conf /etc/supervisor/conf.d/voicely.conf
+
 echo 'export GOPATH="/home/vagrant/go"' >> ~/.bashrc
 echo 'export PATH="$PATH:${GOPATH//://bin:}/bin"' >> ~/.bashrc
 mkdir -p $GOPATH/{bin,pkg,src}
@@ -24,6 +31,8 @@ sudo ln -s /vagrant/conf/nginx.conf /etc/nginx/nginx.conf
 
 mkdir -p $GOPATH/src/github.com/ephemeral-networks/
 sudo ln -s /var/www/ $GOPATH/src/github.com/ephemeral-networks/voicely
+
+cd $GOPATH/src/github.com/ephemeral-networks/voicely && sudo go build -o /usr/local/bin/voicely main.go
 
 touch /vagrant/provisioned
 

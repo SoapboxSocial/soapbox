247f003fd80b2378ca2861f6172447da5f1a918e
decanus <7621705+decanus@users.noreply.github.com>
2020-08-09 21:59:56 +0200

    created table

--- db/tables.sql
+++ db/tables.sql
@@ -7,3 +7,10 @@ CREATE TABLE IF NOT EXISTS users (
 
 CREATE UNIQUE INDEX idx_email ON users (email);
 CREATE UNIQUE INDEX idx_username ON users (username);
+
+CREATE TABLE IF NOT EXISTS followers (
+    follower INT NOT NULL,
+    following INT NOT NULL,
+    FOREIGN KEY (follower) REFERENCES users(id),
+    FOREIGN KEY (following) REFERENCES users(id),
+);

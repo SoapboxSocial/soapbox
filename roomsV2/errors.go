8fb8d323f530db9939b80856a65f84e81599155c
decanus <7621705+decanus@users.noreply.github.com>
2020-08-30 17:52:20 +0200

    do not need

--- roomsV2/errors.go
+++
@@ -1,9 +0,0 @@
-package roomsV2
-
-import "errors"
-
-var (
-	errInvalidSDP           = errors.New("invalid sdp")
-	errSdpParseFailed       = errors.New("sdp parse failed")
-	errConnectionInitFailed = errors.New("peer connection init failed")
-)

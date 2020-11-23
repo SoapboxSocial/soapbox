47999898134d75a8dfd231d262c9f74868c6fa3f
decanus <7621705+decanus@users.noreply.github.com>
2020-11-23 18:06:50 +0100

    uncommented

--- pkg/notifications/limiter/limiter.go
+++ pkg/notifications/limiter/limiter.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/go-redis/redis/v8"
 
-	"github.com/soapboxsocial/soapbox/pkg/notifications"
 	"github.com/soapboxsocial/soapbox/pkg/pubsub"
 	"github.com/soapboxsocial/soapbox/pkg/rooms"
 )
@@ -28,47 +27,45 @@ func NewLimiter(rdb *redis.Client, currentRoom *rooms.CurrentRoomBackend) *Limit
 	}
 }
 
-func (l *Limiter) ShouldSendNotification(target int, args map[string]interface{}, category notifications.NotificationCategory) bool {
+func (l *Limiter) ShouldSendNotification(target int, event *pubsub.Event) bool {
 	// @TODO WE SHOULD LIMIT NEW FOLLOWER NOTIFICATION SO SOMEONE CANT REFOLLOW AND SPAM
-	if category == notifications.NEW_FOLLOWER || category == notifications.GROUP_INVITE {
-		return true
-	}
-
-	if category == notifications.NEW_ROOM {
-		return !l.isLimited(limiterKeyForRoom(target, args["id"].(int)))
-	}
-
-	id := args["id"].(int)
-	room, _ := l.currentRoom.GetCurrentRoomForUser(target)
-	if room == id {
-		return false
-	}
-
-	if category == notifications.ROOM_JOINED {
-		return !l.isLimited(limiterKeyForRoom(target, args["id"].(int)))
-	}
-
-	if category == notifications.ROOM_INVITE {
-		return !l.isLimited(limiterKeyForRoomInvite(target, args["id"].(int)))
-	}
+	//if category == notifications.NEW_FOLLOWER || category == notifications.GROUP_INVITE {
+	//	return true
+	//}
+	//
+	//if category == notifications.NEW_ROOM {
+	//	return !l.isLimited(limiterKeyForRoom(target, args["id"].(int)))
+	//}
+	//
+	//id := args["id"].(int)
+	//room, _ := l.currentRoom.GetCurrentRoomForUser(target)
+	//if room == id {
+	//	return false
+	//}
+	//
+	//if category == notifications.ROOM_JOINED {
+	//	return !l.isLimited(limiterKeyForRoom(target, args["id"].(int)))
+	//}
+	//
+	//if category == notifications.ROOM_INVITE {
+	//	return !l.isLimited(limiterKeyForRoomInvite(target, args["id"].(int)))
+	//}
 
 	return false
 }
 
 func (l *Limiter) SentNotification(target int, event *pubsub.Event) {
-	if event.Type == pubsub.EventTypeNewFollower {
-		return
-	}
 
-	if category == notifications.ROOM_JOINED || category == notifications.NEW_ROOM {
-		l.rdb.Set(l.rdb.Context(), limiterKeyForRoom(target, args["id"].(int)), valueString, keyNotificationCooldown)
-		return
-	}
+	// @TODO CHECK IF KEYS ARE OK?
 
-	if category == notifications.ROOM_INVITE {
-		l.rdb.Set(l.rdb.Context(), limiterKeyForRoomInvite(target, args["id"].(int)), valueString, roomInviteNotificationCooldown)
-		return
-	}
+	//if event.Type == pubsub.EventTypeRoomJoin || event.Type == pubsub.EventTypeNewRoom {
+	//	l.rdb.Set(l.rdb.Context(), limiterKeyForRoom(target, event.Params["id"].(int)), valueString, keyNotificationCooldown)
+	//	return
+	//}
+	//
+	//if event.Type == pubsub.EventTypeRoomInvite {
+	//	l.rdb.Set(l.rdb.Context(), limiterKeyForRoomInvite(target, event.Params["id"].(int)), valueString, roomInviteNotificationCooldown)
+	//}
 }
 
 func (l *Limiter) isLimited(key string) bool {

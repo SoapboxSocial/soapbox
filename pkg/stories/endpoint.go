788272fe66fb149b81dd92f7d04c9472afc3b8cb
decanus <7621705+decanus@users.noreply.github.com>
2020-11-30 15:28:29 +0100

    added endpoint

---
+++ pkg/stories/endpoint.go
@@ -0,0 +1,19 @@
+package stories
+
+import (
+	"github.com/gorilla/mux"
+)
+
+type Endpoint struct {
+
+}
+
+func NewEndpoint() *Endpoint {
+	return &Endpoint{}
+}
+
+func (e *Endpoint) Router() *mux.Router {
+	r := mux.NewRouter()
+
+	return r
+}

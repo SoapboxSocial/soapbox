2058e7208080247df2e43d14bf9a358eadc3807b
decanus <7621705+decanus@users.noreply.github.com>
2020-12-05 13:57:01 +0100

    uncommented for now

--- pkg/stories/endpoint.go
+++ pkg/stories/endpoint.go
@@ -1,7 +1,13 @@
 package stories
 
 import (
+	"log"
+	"net/http"
+	"strconv"
+
 	"github.com/gorilla/mux"
+
+	httputil "github.com/soapboxsocial/soapbox/pkg/http"
 )
 
 type Endpoint struct {
@@ -15,5 +21,41 @@ func NewEndpoint(backend *Backend) *Endpoint {
 func (e *Endpoint) Router() *mux.Router {
 	r := mux.NewRouter()
 
+	r.Path("/upload").Methods("POST").HandlerFunc(e.UploadStory)
+	r.Path("/delete/{id:[0-9]+}").Methods("DELETE").HandlerFunc(e.DeleteStory)
+
 	return r
+}
+
+func (e *Endpoint) UploadStory(w http.ResponseWriter, r *http.Request) {
+	//userID, ok := auth.GetUserIDFromContext(r.Context())
+	//if !ok {
+	//	httputil.JsonError(w, http.StatusInternalServerError, httputil.ErrorCodeInvalidRequestBody, "invalid id")
+	//	return
+	//}
+}
+
+func (e *Endpoint) DeleteStory(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (e *Endpoint) GetStoriesForUser(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+
+	id, err := strconv.Atoi(params["id"])
+	if err != nil {
+		httputil.JsonError(w, http.StatusBadRequest, httputil.ErrorCodeInvalidRequestBody, "invalid id")
+		return
+	}
+
+	stories, err := e.backend.GetStoriesForUser(id)
+	if err != nil {
+		httputil.JsonError(w, http.StatusInternalServerError, httputil.ErrorCodeInvalidRequestBody, "invalid id")
+		return
+	}
+
+	err = httputil.JsonEncode(w, stories)
+	if err != nil {
+		log.Printf("failed to write story response: %s\n", err.Error())
+	}
 }

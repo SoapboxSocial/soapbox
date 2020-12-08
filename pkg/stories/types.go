39cfcde65b55635817f294b046abcb2670875cc6
decanus <7621705+decanus@users.noreply.github.com>
2020-12-08 10:27:34 +0100

    string

--- pkg/stories/types.go
+++ pkg/stories/types.go
@@ -2,7 +2,7 @@ package stories
 
 // Story represents a user story.
 type Story struct {
-	ID              int   `json:"id"`
-	ExpiresAt       int64 `json:"expires_at"`
-	DeviceTimestamp int64 `json:"device_timestamp"`
+	ID              string `json:"id"`
+	ExpiresAt       int64  `json:"expires_at"`
+	DeviceTimestamp int64  `json:"device_timestamp"`
 }

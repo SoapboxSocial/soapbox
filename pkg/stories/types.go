45de18e5b9ae0b0522ea388c127f0a0cdd6ddead
decanus <7621705+decanus@users.noreply.github.com>
2020-12-09 21:28:37 +0100

    reaction

--- pkg/stories/types.go
+++ pkg/stories/types.go
@@ -1,8 +1,15 @@
 package stories
 
+// Reaction represents the reactions users submitted to the story.
+type Reaction struct {
+	Emoji string `json:"emoji"`
+	Count int    `json:"count"`
+}
+
 // Story represents a user story.
 type Story struct {
-	ID              string `json:"id"`
-	ExpiresAt       int64  `json:"expires_at"`
-	DeviceTimestamp int64  `json:"device_timestamp"`
+	ID              string     `json:"id"`
+	ExpiresAt       int64      `json:"expires_at"`
+	DeviceTimestamp int64      `json:"device_timestamp"`
+	Reactions       []Reaction `json:"reactions"`
 }

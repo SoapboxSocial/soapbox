b8de18e6784598e6a11070b8396728cd9a985bd7
decanus <7621705+decanus@users.noreply.github.com>
2021-01-04 22:00:55 +0100

    new server

--- pkg/rooms/server.go
+++ pkg/rooms/server.go
@@ -1,5 +1,33 @@
 package rooms
 
-type Server struct {
+import (
+	"sync"
 
+	"github.com/pion/ion-sfu/pkg/sfu"
+
+	"github.com/soapboxsocial/soapbox/pkg/groups"
+	"github.com/soapboxsocial/soapbox/pkg/pubsub"
+	"github.com/soapboxsocial/soapbox/pkg/rooms/pb"
+	"github.com/soapboxsocial/soapbox/pkg/sessions"
+	"github.com/soapboxsocial/soapbox/pkg/users"
+)
+
+type Server struct {
+	mux sync.RWMutex
+
+	sfu    *sfu.SFU
+	sm     *sessions.SessionManager
+	ub     *users.UserBackend
+	groups *groups.Backend
+	queue  *pubsub.Queue
+
+	currentRoom *CurrentRoomBackend
+
+	rooms map[int]*Room
+
+	nextID int
+}
+
+func (s *Server) Signal(stream pb.SFU_SignalServer) error {
+	return nil
 }

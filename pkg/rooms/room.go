41916e736de51099d31025938e5b4f18d090d3b2
decanus <7621705+decanus@users.noreply.github.com>
2021-01-05 13:15:55 +0100

    removed

--- pkg/rooms/room.go
+++ pkg/rooms/room.go
@@ -4,6 +4,8 @@ import (
 	"sync"
 
 	"github.com/pion/ion-sfu/pkg/sfu"
+
+	"github.com/soapboxsocial/soapbox/pkg/rooms/pb"
 )
 
 type Member struct {
@@ -27,4 +29,33 @@ func (r *Room) PeerCount() int {
 
 func (r *Room) Handle(user int, peer *sfu.Peer) error {
 	return nil
+}
+
+func (r *Room) onMessage(command *pb.Command) {
+	switch command.Payload.(type) {
+	case *pb.Command_Mute_:
+		break
+	case *pb.Command_Unmute_:
+		break
+	case *pb.Command_Reaction_:
+		break
+	case *pb.Command_LinkShare_:
+		break
+	case *pb.Command_InviteAdmin_:
+		break
+	case *pb.Command_AcceptAdmin_:
+		break
+	case *pb.Command_RemoveAdmin_:
+		break
+	case *pb.Command_RenameRoom_:
+		break
+	case *pb.Command_InviteUser_:
+		break
+	case *pb.Command_KickUser_:
+		break
+	case *pb.Command_MuteUser_:
+		break
+	case *pb.Command_RecordScreen_:
+		break
+	}
 }

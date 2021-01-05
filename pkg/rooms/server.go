951d4658525f93ad3ae7fc59e95d6a4adb3d55a6
decanus <7621705+decanus@users.noreply.github.com>
2021-01-05 08:07:32 +0100

    fixes

--- pkg/rooms/server.go
+++ pkg/rooms/server.go
@@ -1,11 +1,14 @@
 package rooms
 
 import (
+	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 	"sync"
 
 	"github.com/pion/ion-sfu/pkg/sfu"
+	"github.com/pion/webrtc/v3"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
@@ -69,4 +72,97 @@ func (s *Server) Signal(stream pb.SFU_SignalServer) error {
 			return status.Error(codes.FailedPrecondition, "invalid message")
 		}
 	}
+}
+
+// setup properly sets up a peer to communicate with the SFU.
+func setup(peer *sfu.Peer, messageId, room string, stream pb.SFU_SignalServer, description webrtc.SessionDescription) error {
+
+	// Notify user of new ice candidate
+	peer.OnIceCandidate = func(candidate *webrtc.ICECandidateInit, target int) {
+		bytes, err := json.Marshal(candidate)
+		if err != nil {
+			log.Printf("OnIceCandidate error %s", err)
+		}
+		err = stream.Send(&pb.SignalReply{
+			Payload: &pb.SignalReply_Trickle{
+				Trickle: &pb.Trickle{
+					Init:   string(bytes),
+					Target: pb.Trickle_Target(target),
+				},
+			},
+		})
+		if err != nil {
+			log.Printf("OnIceCandidate send error %v ", err)
+		}
+	}
+
+	// Notify user of new offer
+	peer.OnOffer = func(o *webrtc.SessionDescription) {
+		description := &pb.SessionDescription{
+			Type: o.Type.String(),
+			Sdp: []byte(o.SDP),
+		}
+
+		err := stream.Send(&pb.SignalReply{
+			Payload: &pb.SignalReply_Description{
+				Description: description,
+			},
+		})
+
+		if err != nil {
+			log.Printf("negotiation error %s", err)
+		}
+	}
+
+	peer.OnICEConnectionStateChange = func(c webrtc.ICEConnectionState) {
+		err := stream.Send(&pb.SignalReply{
+			Payload: &pb.SignalReply_IceConnectionState{
+				IceConnectionState: c.String(),
+			},
+		})
+
+		if err != nil {
+			log.Printf("oniceconnectionstatechange error %s", err)
+		}
+	}
+
+	answer, err := peer.Join(room, description)
+	if err != nil {
+		switch err {
+		case sfu.ErrTransportExists:
+			fallthrough
+		case sfu.ErrOfferIgnored:
+			err = stream.Send(&pb.SignalReply{
+				Payload: &pb.SignalReply_Error{
+					Error: fmt.Errorf("join error: %w", err).Error(),
+				},
+			})
+			if err != nil {
+				log.Printf("grpc send error %v ", err)
+				return status.Errorf(codes.Internal, err.Error())
+			}
+		default:
+			return status.Errorf(codes.Unknown, err.Error())
+		}
+	}
+
+	// send answer
+	err = stream.Send(&pb.SignalReply{
+		Id: messageId,
+		Payload: &pb.SignalReply_Join{
+			Join: &pb.JoinReply{
+				Description: &pb.SessionDescription{
+					Type: answer.Type.String(),
+					Sdp: []byte(answer.SDP),
+				},
+			},
+		},
+	})
+
+	if err != nil {
+		log.Printf("error sending join response %s", err)
+		return status.Errorf(codes.Internal, "join error %s", err)
+	}
+
+	return nil
 }

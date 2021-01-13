48047f435c6bf74205c0488a7f87799d04a14cad
decanus <7621705+decanus@users.noreply.github.com>
2021-01-13 23:30:52 +0100

    added queries

--- pkg/blocks/backend.go
+++ pkg/blocks/backend.go
@@ -36,10 +36,54 @@ func (b *Backend) UnblockUser(user, block int) error {
 	return nil
 }
 
-func (b *Backend) GetUsersWhoBlocked(user int) error {
-	return nil
+func (b *Backend) GetUsersWhoBlocked(user int) ([]int, error) {
+	stmt, err := b.db.Prepare("SELECT user_id FROM blocks WHERE blocked = $1;")
+	if err != nil {
+		return nil, err
+	}
+
+	rows, err := stmt.Query(user)
+	if err != nil {
+		return nil, err
+	}
+
+	result := make([]int, 0)
+
+	for rows.Next() {
+		var blocker int
+		err := rows.Scan(&blocker)
+		if err != nil {
+			return nil, err
+		}
+
+		result = append(result, blocker)
+	}
+
+	return result, nil
 }
 
-func (b *Backend) GetUsersBlockedBy(user int) error {
-	return nil
+func (b *Backend) GetUsersBlockedBy(user int) ([]int, error) {
+	stmt, err := b.db.Prepare("SELECT blocked FROM blocks WHERE user_id = $1;")
+	if err != nil {
+		return nil, err
+	}
+
+	rows, err := stmt.Query(user)
+	if err != nil {
+		return nil, err
+	}
+
+	result := make([]int, 0)
+
+	for rows.Next() {
+		var blocker int
+		err := rows.Scan(&blocker)
+		if err != nil {
+			return nil, err
+		}
+
+		result = append(result, blocker)
+	}
+
+	return result, nil
 }

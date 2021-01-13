2e190a7bf1cd3d3e21211a36d94da7921fc1d304
decanus <7621705+decanus@users.noreply.github.com>
2021-01-13 23:21:24 +0100

    fixes

--- pkg/blocks/backend.go
+++ pkg/blocks/backend.go
@@ -9,10 +9,30 @@ type Backend struct {
 }
 
 func (b *Backend) BlockUser(user, block int) error {
+	stmt, err := b.db.Prepare("INSERT INTO blocks (user_id, blocked) VALUES ($1, $2);")
+	if err != nil {
+		return err
+	}
+
+	_, err = stmt.Exec(user, block)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
 func (b *Backend) UnblockUser(user, block int) error {
+	stmt, err := b.db.Prepare("DELETE FROM blocks WHERE user_id = $1 AND blocked = $2;")
+	if err != nil {
+		return err
+	}
+
+	_, err = stmt.Exec(user, block)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 

755522053a635f0e175edd0e93bb03899ebd99df
decanus <7621705+decanus@users.noreply.github.com>
2021-01-24 15:33:42 +0100

    fixes

--- pkg/login/endpoint_test.go
+++ pkg/login/endpoint_test.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"os"
 	"strings"
 	"testing"
@@ -40,16 +41,17 @@ func TestLoginEndpoint_LoginWithTestAccount(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	redisClient := redis.NewClient(&redis.Options{
+	rdb := redis.NewClient(&redis.Options{
 		Addr: mr.Addr(),
 	})
 
 	endpoint := login.NewEndpoint(
 		users.NewUserBackend(db),
-		sessions.NewSessionManager(redisClient),
+		login.NewStateManager(rdb),
+		sessions.NewSessionManager(rdb),
 		mail.NewMailService(&sendgrid.Client{}),
 		images.NewImagesBackend("/foo"),
-		pubsub.NewQueue(redisClient),
+		pubsub.NewQueue(rdb),
 	)
 
 	rr := httptest.NewRecorder()
@@ -65,7 +67,64 @@ func TestLoginEndpoint_LoginWithTestAccount(t *testing.T) {
 
 	handler.ServeHTTP(rr, req)
 
-	log.Print(rr.Body)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+}
+
+func TestLoginEndpoint_PinSubmission(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	mr, err := miniredis.Run()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr: mr.Addr(),
+	})
+
+	state := login.NewStateManager(rdb)
+
+	endpoint := login.NewEndpoint(
+		users.NewUserBackend(db),
+		state,
+		sessions.NewSessionManager(rdb),
+		mail.NewMailService(&sendgrid.Client{}),
+		images.NewImagesBackend("/foo"),
+		pubsub.NewQueue(rdb),
+	)
+
+	rr := httptest.NewRecorder()
+	handler := endpoint.Router()
+
+	token := "1234"
+	pin := "123456"
+	email := "test@apple.com"
+
+	err = state.SetPinState(token, email, pin)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	mock.ExpectPrepare("^SELECT (.+)").ExpectQuery().
+		WithArgs(email).WillReturnRows(mock.NewRows([]string{"id", "display_name", "username", "image", "bio", "email"}).FromCSVString("1,dean,dean,123.png,my bio,test@apple.com"))
+
+	form := url.Values{}
+	form.Add("pin", pin)
+	form.Add("token", token)
+
+	req, err := http.NewRequest("POST", "/pin", strings.NewReader(form.Encode()))
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	handler.ServeHTTP(rr, req)
 
 	if status := rr.Code; status != http.StatusOK {
 		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)

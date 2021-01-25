a308dbd751bc9c4fb8ba1037cd87a18b502dc914
decanus <7621705+decanus@users.noreply.github.com>
2021-01-25 20:12:12 +0100

    rm

--- pkg/rooms/room.go
+++ pkg/rooms/room.go
@@ -297,9 +297,6 @@ func (r *Room) notify(event *pb.Event) {
 		err := member.Notify(CHANNEL, data)
 		if err != nil {
 			log.Printf("failed to notify: %v\n", err)
-			continue
 		}
-
-		log.Printf("notified %d", id)
 	}
 }

36fd5809b2d289038e84d303ff37511a3bdc1916
decanus <7621705+decanus@users.noreply.github.com>
2021-02-10 16:52:48 +0100

    shortened

--- pkg/conf/conf_test.go
+++ pkg/conf/conf_test.go
@@ -1,50 +1,58 @@
 package conf_test
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/soapboxsocial/soapbox/pkg/conf"
 )
 
 func TestLoad(t *testing.T) {
-
-	c := &conf.RedisConf{}
-	err := conf.Load("./testdata/redis.toml", c)
-	if err != nil {
-		t.Fatal(err)
+	var conftests = []struct{
+		in string
+		err bool
+		conf *conf.RedisConf
+	}{
+		{
+			"./testdata/redis.toml",
+			false,
+			&conf.RedisConf{
+				Database: 12,
+				Port: 1234,
+				Password: "test",
+				Host: "test",
+			},
+		},
+		{
+			"./testdata/invalid.toml",
+			true,
+			nil,
+		},
+		{
+			"./testdata/wow.toml",
+			true,
+			nil,
+		},
 	}
 
-	if c.Database != 12 {
-		t.Fatalf("unexpected database %d", c.Database)
-	}
+	for _, tt := range conftests {
+		t.Run(tt.in, func(t *testing.T) {
+			c := &conf.RedisConf{}
+			err := conf.Load(tt.in, c)
 
-	if c.Port != 1234 {
-		t.Fatalf("unexpected port %d", c.Port)
-	}
+			if err != nil {
+				if tt.err {
+					return
+				} else {
+					t.Fatalf("unexpected err %s", err)
+					return
+				}
+			}
 
-	if c.Password != "test" {
-		t.Fatalf("unexpected password %s", c.Password)
-	}
 
-	if c.Host != "test" {
-		t.Fatalf("unexpected host %s", c.Host)
-	}
-}
-
-func TestLoad_FailureWithInvalidConf(t *testing.T) {
-
-	c := &conf.RedisConf{}
-	err := conf.Load("./testdata/invalid.toml", c)
-	if err == nil {
-		t.Fatal("loading did not fail")
-	}
-}
-
-func TestLoad_FailureWithInvalidPath(t *testing.T) {
-
-	c := &conf.RedisConf{}
-	err := conf.Load("./testdata/wow.toml", c)
-	if err == nil {
-		t.Fatal("loading did not fail")
+			if !reflect.DeepEqual(c, tt.conf) {
+				t.Fatalf("config %v does not match %v", c, tt.conf)
+			}
+		})
 	}
 }

b2036856a0864ca537befa102b27d01b4e13a433
decanus <7621705+decanus@users.noreply.github.com>
2021-02-24 16:08:00 +0100

    format

--- pkg/minis/types.go
+++ pkg/minis/types.go
@@ -1,9 +1,9 @@
 package minis
 
 type Mini struct {
-	ID    int    `json:"id"`
-	Name  string `json:"name"`
-	Image string `json:"image"`
-	Slug  string `json:"slug"`
-	Description  string `json:"description"`
+	ID          int    `json:"id"`
+	Name        string `json:"name"`
+	Image       string `json:"image"`
+	Slug        string `json:"slug"`
+	Description string `json:"description"`
 }

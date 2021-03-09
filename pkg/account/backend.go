47f7768b0bc083dda4a5bc696cb445a0a4aec724
decanus <7621705+decanus@users.noreply.github.com>
2021-03-09 16:28:06 +0000

    fix

--- pkg/account/backend.go
+++ pkg/account/backend.go
@@ -1,9 +1,27 @@
 package account
 
-type Backend struct {
+import "database/sql"
 
+type Backend struct {
+	db *sql.DB
+}
+
+func NewBackend(db *sql.DB) *Backend {
+	return &Backend{
+		db: db,
+	}
 }
 
 func (b *Backend) DeleteAccount(id int) error {
+	stmt, err := b.db.Prepare("DELETE FROM users WHERE id = $1")
+	if err != nil {
+		return err
+	}
+
+	_, err = stmt.Exec(id)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }

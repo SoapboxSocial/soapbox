35a128202a2129e6423af65c6fb7482bcc9fa8da
decanus <7621705+decanus@users.noreply.github.com>
2021-03-19 17:11:45 +0000

    fix

--- cmd/tracking/main.go
+++ cmd/tracking/main.go
@@ -3,14 +3,13 @@ package main
 import (
 	"flag"
 	"log"
-	"strconv"
 
 	"github.com/dukex/mixpanel"
 
 	"github.com/soapboxsocial/soapbox/pkg/conf"
 	"github.com/soapboxsocial/soapbox/pkg/pubsub"
 	"github.com/soapboxsocial/soapbox/pkg/redis"
-	"github.com/soapboxsocial/soapbox/pkg/tracking"
+	"github.com/soapboxsocial/soapbox/pkg/tracking/trackers"
 )
 
 type Conf struct {
@@ -42,7 +41,7 @@ func main() {
 	}
 
 	client := mixpanel.New(config.Mixpanel.Token, config.Mixpanel.URL)
-	tracker := tracking.NewMixpanelTracker(client)
+	tracker := trackers.NewMixpanelTracker(client)
 
 	rdb := redis.NewRedis(config.Redis)
 	queue := pubsub.NewQueue(rdb)
@@ -50,207 +49,13 @@ func main() {
 	events := queue.Subscribe(pubsub.RoomTopic, pubsub.UserTopic, pubsub.GroupTopic, pubsub.StoryTopic)
 
 	for evt := range events {
-		event := handleEvent(evt)
-		if event == nil {
+		if !tracker.CanTrack(evt) {
 			continue
 		}
 
-		go func() {
-			err := tracker.Track(event)
-			if err != nil {
-				log.Printf("tracker.Track err %v", err)
-			}
-		}()
-	}
-}
-
-func handleEvent(event *pubsub.Event) *tracking.Event {
-	switch event.Type {
-	case pubsub.EventTypeNewRoom:
-
-		id, err := event.GetInt("creator")
+		err := tracker.Track(evt)
 		if err != nil {
-			return nil
-		}
-
-		return &tracking.Event{
-			ID:   strconv.Itoa(id),
-			Name: "room_new",
-			Properties: map[string]interface{}{
-				"room_id":    event.Params["id"],
-				"visibility": event.Params["visibility"],
-			},
-		}
-	case pubsub.EventTypeRoomJoin:
-		id, err := event.GetInt("creator")
-		if err != nil {
-			return nil
-		}
-
-		return &tracking.Event{
-			ID:   strconv.Itoa(id),
-			Name: "room_join",
-			Properties: map[string]interface{}{
-				"room_id":    event.Params["id"],
-				"visibility": event.Params["visibility"],
-			},
-		}
-	case pubsub.EventTypeRoomLeft:
-		id, err := event.GetInt("creator")
-		if err != nil {
-			return nil
-		}
-
-		return &tracking.Event{
-			ID:   strconv.Itoa(id),
-			Name: "room_left",
-			Properties: map[string]interface{}{
-				"room_id": event.Params["id"],
-			},
-		}
-	case pubsub.EventTypeNewUser:
-		id, err := event.GetInt("id")
-		if err != nil {
-			return nil
-		}
-
-		return &tracking.Event{
-			ID:   strconv.Itoa(id),
-			Name: tracking.NewUser,
-			Properties: map[string]interface{}{
-				"user_id":  event.Params["id"],
-				"username": event.Params["username"],
-			},
-		}
-	case pubsub.EventTypeNewGroup:
-		id, err := event.GetInt("creator")
-		if err != nil {
-			return nil
-		}
-
-		return &tracking.Event{
-			ID:   strconv.Itoa(id),
-			Name: "group_new",
-			Properties: map[string]interface{}{
-				"group_id": event.Params["id"],
-				"name":     event.Params["name"],
-			},
-		}
-	case pubsub.EventTypeNewGroupRoom:
-		id, err := event.GetInt("creator")
-		if err != nil {
-			return nil
-		}
-
-		return &tracking.Event{
-			ID:   strconv.Itoa(id),
-			Name: "room_new",
-			Properties: map[string]interface{}{
-				"room_id":    event.Params["id"],
-				"visibility": event.Params["visibility"],
-				"group_id":   event.Params["group"],
-			},
-		}
-	case pubsub.EventTypeNewFollower:
-		id, err := event.GetInt("follower")
-		if err != nil {
-			return nil
-		}
-
-		return &tracking.Event{
-			ID:   strconv.Itoa(id),
-			Name: "followed",
-			Properties: map[string]interface{}{
-				"following_id": event.Params["id"],
-			},
-		}
-	case pubsub.EventTypeGroupJoin:
-		id, err := event.GetInt("id")
-		if err != nil {
-			return nil
-		}
-
-		return &tracking.Event{
-			ID:   strconv.Itoa(id),
-			Name: "group_join",
-			Properties: map[string]interface{}{
-				"group": event.Params["group"],
-			},
-		}
-	case pubsub.EventTypeNewStory:
-		id, err := event.GetInt("creator")
-		if err != nil {
-			return nil
-		}
-
-		return &tracking.Event{
-			ID:         strconv.Itoa(id),
-			Name:       "story_new",
-			Properties: map[string]interface{}{},
-		}
-	case pubsub.EventTypeStoryReaction:
-		id, err := event.GetInt("id")
-		if err != nil {
-			return nil
-		}
-
-		return &tracking.Event{
-			ID:         strconv.Itoa(id),
-			Name:       "story_reaction",
-			Properties: map[string]interface{}{},
-		}
-	case pubsub.EventTypeUserHeartbeat:
-
-		// @TODO ADD HEARTBEAT COOLDOWN of 30 mins
-
-		id, err := event.GetInt("id")
-		if err != nil {
-			return nil
-		}
-
-		return &tracking.Event{
-			ID:         strconv.Itoa(id),
-			Name:       "heartbeat",
-			Properties: map[string]interface{}{},
-		}
-	case pubsub.EventTypeRoomOpenMini:
-		id, err := event.GetInt("id")
-		if err != nil {
-			return nil
-		}
-
-		return &tracking.Event{
-			ID:   strconv.Itoa(id),
-			Name: "room_open_mini",
-			Properties: map[string]interface{}{
-				"room_id": event.Params["room"],
-				"mini":    event.Params["mini"],
-			},
-		}
-	case pubsub.EventTypeRoomLinkShare:
-		id, err := event.GetInt("id")
-		if err != nil {
-			return nil
-		}
-
-		return &tracking.Event{
-			ID:   strconv.Itoa(id),
-			Name: "room_link_share",
-			Properties: map[string]interface{}{
-				"room_id": event.Params["room"],
-			},
-		}
-	case pubsub.EventTypeDeleteUser:
-		id, err := event.GetInt("id")
-		if err != nil {
-			return nil
-		}
-
-		return &tracking.Event{
-			ID:   strconv.Itoa(id),
-			Name: tracking.DeleteUser,
+			log.Printf("tacker.Track err %v", err)
 		}
 	}
-
-	return nil
 }
---
+++ pkg/tracking/trackers/mixpaneltracker.go
@@ -0,0 +1,257 @@
+package trackers
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/dukex/mixpanel"
+
+	"github.com/soapboxsocial/soapbox/pkg/pubsub"
+	"github.com/soapboxsocial/soapbox/pkg/tracking"
+)
+
+const (
+	NewUser    = "new_user"
+	DeleteUser = "delete_user"
+)
+
+type MixpanelTracker struct {
+	client mixpanel.Mixpanel
+}
+
+func NewMixpanelTracker(client mixpanel.Mixpanel) *MixpanelTracker {
+	return &MixpanelTracker{client: client}
+}
+
+func (m *MixpanelTracker) CanTrack(event *pubsub.Event) bool {
+	return event.Type != pubsub.EventTypeRoomInvite &&
+		event.Type != pubsub.EventTypeUserUpdate &&
+		event.Type != pubsub.EventTypeGroupInvite &&
+		event.Type != pubsub.EventTypeGroupUpdate &&
+		event.Type != pubsub.EventTypeGroupDelete &&
+		event.Type != pubsub.EventTypeWelcomeRoom
+}
+
+func (m *MixpanelTracker) Track(event *pubsub.Event) error {
+	log := transform(event)
+	if log == nil {
+		return fmt.Errorf("invalid type for tracker: %d", event.Type)
+	}
+
+	if log.Name == DeleteUser {
+		return m.client.Update(log.ID, &mixpanel.Update{
+			IP:        "0",
+			Operation: "$delete",
+		})
+	}
+
+	err := m.client.Track(log.ID, log.Name, &mixpanel.Event{IP: "0", Properties: log.Properties})
+	if err != nil {
+		return err
+	}
+
+	if log.Name == NewUser {
+		err := m.client.Update(log.ID, &mixpanel.Update{
+			IP:         "0",
+			Operation:  "$set",
+			Properties: log.Properties,
+		})
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func transform(event *pubsub.Event) *tracking.Event {
+	switch event.Type {
+	case pubsub.EventTypeNewRoom:
+
+		id, err := event.GetInt("creator")
+		if err != nil {
+			return nil
+		}
+
+		return &tracking.Event{
+			ID:   strconv.Itoa(id),
+			Name: "room_new",
+			Properties: map[string]interface{}{
+				"room_id":    event.Params["id"],
+				"visibility": event.Params["visibility"],
+			},
+		}
+	case pubsub.EventTypeRoomJoin:
+		id, err := event.GetInt("creator")
+		if err != nil {
+			return nil
+		}
+
+		return &tracking.Event{
+			ID:   strconv.Itoa(id),
+			Name: "room_join",
+			Properties: map[string]interface{}{
+				"room_id":    event.Params["id"],
+				"visibility": event.Params["visibility"],
+			},
+		}
+	case pubsub.EventTypeRoomLeft:
+		id, err := event.GetInt("creator")
+		if err != nil {
+			return nil
+		}
+
+		return &tracking.Event{
+			ID:   strconv.Itoa(id),
+			Name: "room_left",
+			Properties: map[string]interface{}{
+				"room_id": event.Params["id"],
+			},
+		}
+	case pubsub.EventTypeNewUser:
+		id, err := event.GetInt("id")
+		if err != nil {
+			return nil
+		}
+
+		return &tracking.Event{
+			ID:   strconv.Itoa(id),
+			Name: NewUser,
+			Properties: map[string]interface{}{
+				"user_id":  event.Params["id"],
+				"username": event.Params["username"],
+			},
+		}
+	case pubsub.EventTypeNewGroup:
+		id, err := event.GetInt("creator")
+		if err != nil {
+			return nil
+		}
+
+		return &tracking.Event{
+			ID:   strconv.Itoa(id),
+			Name: "group_new",
+			Properties: map[string]interface{}{
+				"group_id": event.Params["id"],
+				"name":     event.Params["name"],
+			},
+		}
+	case pubsub.EventTypeNewGroupRoom:
+		id, err := event.GetInt("creator")
+		if err != nil {
+			return nil
+		}
+
+		return &tracking.Event{
+			ID:   strconv.Itoa(id),
+			Name: "room_new",
+			Properties: map[string]interface{}{
+				"room_id":    event.Params["id"],
+				"visibility": event.Params["visibility"],
+				"group_id":   event.Params["group"],
+			},
+		}
+	case pubsub.EventTypeNewFollower:
+		id, err := event.GetInt("follower")
+		if err != nil {
+			return nil
+		}
+
+		return &tracking.Event{
+			ID:   strconv.Itoa(id),
+			Name: "followed",
+			Properties: map[string]interface{}{
+				"following_id": event.Params["id"],
+			},
+		}
+	case pubsub.EventTypeGroupJoin:
+		id, err := event.GetInt("id")
+		if err != nil {
+			return nil
+		}
+
+		return &tracking.Event{
+			ID:   strconv.Itoa(id),
+			Name: "group_join",
+			Properties: map[string]interface{}{
+				"group": event.Params["group"],
+			},
+		}
+	case pubsub.EventTypeNewStory:
+		id, err := event.GetInt("creator")
+		if err != nil {
+			return nil
+		}
+
+		return &tracking.Event{
+			ID:         strconv.Itoa(id),
+			Name:       "story_new",
+			Properties: map[string]interface{}{},
+		}
+	case pubsub.EventTypeStoryReaction:
+		id, err := event.GetInt("id")
+		if err != nil {
+			return nil
+		}
+
+		return &tracking.Event{
+			ID:         strconv.Itoa(id),
+			Name:       "story_reaction",
+			Properties: map[string]interface{}{},
+		}
+	case pubsub.EventTypeUserHeartbeat:
+
+		// @TODO ADD HEARTBEAT COOLDOWN of 30 mins
+
+		id, err := event.GetInt("id")
+		if err != nil {
+			return nil
+		}
+
+		return &tracking.Event{
+			ID:         strconv.Itoa(id),
+			Name:       "heartbeat",
+			Properties: map[string]interface{}{},
+		}
+	case pubsub.EventTypeRoomOpenMini:
+		id, err := event.GetInt("id")
+		if err != nil {
+			return nil
+		}
+
+		return &tracking.Event{
+			ID:   strconv.Itoa(id),
+			Name: "room_open_mini",
+			Properties: map[string]interface{}{
+				"room_id": event.Params["room"],
+				"mini":    event.Params["mini"],
+			},
+		}
+	case pubsub.EventTypeRoomLinkShare:
+		id, err := event.GetInt("id")
+		if err != nil {
+			return nil
+		}
+
+		return &tracking.Event{
+			ID:   strconv.Itoa(id),
+			Name: "room_link_share",
+			Properties: map[string]interface{}{
+				"room_id": event.Params["room"],
+			},
+		}
+	case pubsub.EventTypeDeleteUser:
+		id, err := event.GetInt("id")
+		if err != nil {
+			return nil
+		}
+
+		return &tracking.Event{
+			ID:   strconv.Itoa(id),
+			Name: DeleteUser,
+		}
+	}
+
+	return nil
+}

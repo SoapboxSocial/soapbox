2908077b80d0d85f7a734c43d9c90eb44abbbb88
decanus <7621705+decanus@users.noreply.github.com>
2021-04-02 11:48:51 +0200

    init

--- pkg/tracking/trackers/recentlyactivetracker.go
+++ pkg/tracking/trackers/recentlyactivetracker.go
@@ -1,10 +1,18 @@
 package trackers
 
 import (
+	"github.com/soapboxsocial/soapbox/pkg/activeusers"
 	"github.com/soapboxsocial/soapbox/pkg/pubsub"
 )
 
 type RecentlyActiveTracker struct {
+	backend *activeusers.Backend
+}
+
+func NewRecentlyActiveTracker(backend *activeusers.Backend) *RecentlyActiveTracker {
+	return &RecentlyActiveTracker{
+		backend: backend,
+	}
 }
 
 func (r *RecentlyActiveTracker) CanTrack(event *pubsub.Event) bool {

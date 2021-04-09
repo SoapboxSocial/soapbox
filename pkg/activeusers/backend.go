347989f203820a533903390e20f5958a3f0b4fcd
decanus <7621705+decanus@users.noreply.github.com>
2021-04-09 15:27:06 +0200

    update

--- pkg/activeusers/backend.go
+++ pkg/activeusers/backend.go
@@ -24,3 +24,39 @@ func (b *Backend) SetLastActiveTime(user int, time time.Time) error {
 	_, err = stmt.Exec(user, time)
 	return err
 }
+
+func (b *Backend) GetActiveUsersForFollower(user int) ([]ActiveUser, error) {
+	query := `SELECT users.id, users.display_name, users.username, users.image, active.room FROM users
+		INNER JOIN (SELECT * FROM current_rooms UNION (SELECT user_id, '' FROM user_active_times WHERE last_active > (NOW() - INTERVAL '30 MINUTE'))) active ON users.id = active.user_id
+		INNER JOIN followers ON (a.user_id = followers.user_id) WHERE followers.follower = $1;`
+
+	stmt, err := b.db.Prepare(query)
+	if err != nil {
+		return nil, err
+	}
+
+	rows, err := stmt.Query(user)
+	if err != nil {
+		return nil, err
+	}
+
+	result := make([]ActiveUser, 0)
+
+	for rows.Next() {
+		user := ActiveUser{}
+		var room string
+
+		err := rows.Scan(&user.ID, &user.DisplayName, &user.Username, &user.Image, &room)
+		if err != nil {
+			continue
+		}
+
+		if room != "" {
+			user.Room = &room
+		}
+
+		result = append(result, user)
+	}
+
+	return result, nil
+}

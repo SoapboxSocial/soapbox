b73af5ca2843e68b9ef58c425f735838d42663e5
decanus <7621705+decanus@users.noreply.github.com>
2021-04-29 15:33:54 +0200

    moved to workers

--- pkg/notifications/worker/worker_test.go
+++ pkg/notifications/worker/worker_test.go
@@ -37,8 +37,7 @@ func TestWorker(t *testing.T) {
 	apns := mocks.NewMockAPNS(ctrl)
 
 	pool := make(chan chan worker.Job)
-
-	service := worker.NewWorker(
+	w := worker.NewWorker(
 		pool,
 		&worker.Config{
 			APNS:    apns,
@@ -67,81 +66,88 @@ func TestWorker(t *testing.T) {
 		WithArgs(id).
 		WillReturnRows(mock.NewRows([]string{"token"}).FromCSVString(device))
 
-	done := make(chan bool)
-	apns.EXPECT().Send(gomock.Eq(device), gomock.Any()).Return(nil).Do(func(string, notifications.PushNotification) {
-		done <- true
-	})
+	apns.EXPECT().Send(gomock.Eq(device), gomock.Any()).Return(nil)
 
-	service.Start()
+	w.Start()
 
-	w := <-pool
+	queue := <-pool
 
-	w <- worker.Job{
+	queue <- worker.Job{
 		Target:       notifications.Target{ID: id, RoomFrequency: notifications.Frequent, Follows: true},
 		Notification: &notification,
 	}
 
-	<-done
+	<-pool
 }
 
-//
-//func TestService_Send_WithUnregistered(t *testing.T) {
-//	mr, err := miniredis.Run()
-//	if err != nil {
-//		t.Fatal(err)
-//	}
-//
-//	rdb := redis.NewClient(&redis.Options{
-//		Addr: mr.Addr(),
-//	})
-//
-//	db, mock, err := sqlmock.New()
-//	if err != nil {
-//		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
-//	}
-//	defer db.Close()
-//
-//	ctrl := gomock.NewController(t)
-//	defer ctrl.Finish()
-//
-//	apns := mocks.NewMockAPNS(ctrl)
-//
-//	service := notifications.NewService(
-//		apns,
-//		notifications.NewLimiter(rdb, rooms.NewCurrentRoomBackend(db)),
-//		devices.NewBackend(db),
-//		notifications.NewStorage(rdb),
-//	)
-//
-//	id := 1
-//	device := "1234"
-//	notification := notifications.PushNotification{
-//		Category:  notifications.ROOM_JOINED,
-//		Arguments: map[string]interface{}{"creator": 1, "id": "123"},
-//	}
-//
-//	mock.
-//		ExpectPrepare("^SELECT (.+)").
-//		ExpectQuery().
-//		WithArgs(id).
-//		WillReturnRows(mock.NewRows([]string{"room"}).FromCSVString("0"))
-//
-//	mock.
-//		ExpectPrepare("^SELECT (.+)").
-//		ExpectQuery().
-//		WithArgs(id).
-//		WillReturnRows(mock.NewRows([]string{"token"}).FromCSVString(device))
-//
-//	apns.EXPECT().Send(gomock.Eq(device), gomock.Any()).Return(notifications.ErrDeviceUnregistered)
-//
-//	mock.
-//		ExpectPrepare("^DELETE (.+)").
-//		ExpectExec().
-//		WithArgs(id).
-//		WillReturnResult(sqlmock.NewResult(1, 1))
-//
-//	service.Send(
-//		notifications.Target{ID: id, RoomFrequency: notifications.Frequent, Follows: true},
-//		&notification,
-//	)
-//}
+
+func TestWorker_WithUnregistered(t *testing.T) {
+	mr, err := miniredis.Run()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr: mr.Addr(),
+	})
+
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	apns := mocks.NewMockAPNS(ctrl)
+
+	pool := make(chan chan worker.Job)
+	w := worker.NewWorker(
+		pool,
+		&worker.Config{
+			APNS:    apns,
+			Limiter: notifications.NewLimiter(rdb, rooms.NewCurrentRoomBackend(db)),
+			Devices: devices.NewBackend(db),
+			Store:   notifications.NewStorage(rdb),
+		},
+	)
+
+	id := 1
+	device := "1234"
+	notification := notifications.PushNotification{
+		Category:  notifications.ROOM_JOINED,
+		Arguments: map[string]interface{}{"creator": 1, "id": "123"},
+	}
+
+	mock.
+		ExpectPrepare("^SELECT (.+)").
+		ExpectQuery().
+		WithArgs(id).
+		WillReturnRows(mock.NewRows([]string{"room"}).FromCSVString("0"))
+
+	mock.
+		ExpectPrepare("^SELECT (.+)").
+		ExpectQuery().
+		WithArgs(id).
+		WillReturnRows(mock.NewRows([]string{"token"}).FromCSVString(device))
+
+	apns.EXPECT().Send(gomock.Eq(device), gomock.Any()).Return(notifications.ErrDeviceUnregistered)
+
+	mock.
+		ExpectPrepare("^DELETE (.+)").
+		ExpectExec().
+		WithArgs(id).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	w.Start()
+
+	queue := <-pool
+
+	queue <- worker.Job{
+		Target:       notifications.Target{ID: id, RoomFrequency: notifications.Frequent, Follows: true},
+		Notification: &notification,
+	}
+
+	<-pool
+}

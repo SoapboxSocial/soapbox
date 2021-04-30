5b6b398a3e848b9fcdaa38d1e3fa987950d33df8
decanus <7621705+decanus@users.noreply.github.com>
2021-04-30 14:41:11 +0200

    fix

--- cmd/notifications/cmd/worker.go
+++ cmd/notifications/cmd/worker.go
@@ -4,6 +4,7 @@ import (
 	sqldb "database/sql"
 	"fmt"
 	"log"
+	"net"
 
 	"github.com/pkg/errors"
 	"github.com/sideshow/apns2"
@@ -15,12 +16,14 @@ import (
 	"github.com/soapboxsocial/soapbox/pkg/conf"
 	"github.com/soapboxsocial/soapbox/pkg/devices"
 	"github.com/soapboxsocial/soapbox/pkg/notifications"
+	notificationsGRPC "github.com/soapboxsocial/soapbox/pkg/notifications/grpc"
 	"github.com/soapboxsocial/soapbox/pkg/notifications/handlers"
+	"github.com/soapboxsocial/soapbox/pkg/notifications/pb"
 	"github.com/soapboxsocial/soapbox/pkg/notifications/worker"
 	"github.com/soapboxsocial/soapbox/pkg/pubsub"
 	"github.com/soapboxsocial/soapbox/pkg/redis"
 	"github.com/soapboxsocial/soapbox/pkg/rooms"
-	"github.com/soapboxsocial/soapbox/pkg/rooms/pb"
+	roompb "github.com/soapboxsocial/soapbox/pkg/rooms/pb"
 	"github.com/soapboxsocial/soapbox/pkg/sql"
 	"github.com/soapboxsocial/soapbox/pkg/users"
 )
@@ -30,7 +33,7 @@ type Conf struct {
 	Redis conf.RedisConf    `mapstructure:"redis"`
 	DB    conf.PostgresConf `mapstructure:"db"`
 	Rooms conf.AddrConf     `mapstructure:"rooms"`
-	GRPC conf.AddrConf     `mapstructure:"GRPC"`
+	GRPC  conf.AddrConf     `mapstructure:"GRPC"`
 }
 
 var workerCmd = &cobra.Command{
@@ -75,7 +78,8 @@ func runWorker(*cobra.Command, []string) error {
 		TeamID:  config.APNS.TeamID,
 	}).Production()
 
-	notificationHandlers := setupHandlers(db, config.Rooms)
+	settings := notifications.NewSettings(db)
+	notificationHandlers := setupHandlers(db, config.Rooms, settings)
 
 	events := queue.Subscribe(pubsub.RoomTopic, pubsub.UserTopic)
 
@@ -119,22 +123,41 @@ func runWorker(*cobra.Command, []string) error {
 		}(event)
 	}
 
+	return runServer(config.GRPC, dispatch, settings)
+}
+
+func runServer(addr conf.AddrConf, dispatcher *worker.Dispatcher, settings *notifications.Settings) error {
+	lis, err := net.Listen("tcp", fmt.Sprintf("%s:%d", addr.Host, addr.Port))
+	if err != nil {
+		return errors.Wrap(err, "failed to start server")
+	}
+
+	gs := grpc.NewServer()
+	pb.RegisterNotificationServiceServer(
+		gs,
+		notificationsGRPC.NewService(dispatcher, settings),
+	)
+
+	err = gs.Serve(lis)
+	if err != nil {
+		log.Panicf("failed to serve: %v", err)
+	}
+
 	return nil
 }
 
-func setupHandlers(db *sqldb.DB, roomsAddr conf.AddrConf) map[pubsub.EventType]handlers.Handler {
+func setupHandlers(db *sqldb.DB, roomsAddr conf.AddrConf, settings *notifications.Settings) map[pubsub.EventType]handlers.Handler {
 	userBackend := users.NewUserBackend(db)
-	targets := notifications.NewSettings(db)
 
 	notificationHandlers := make(map[pubsub.EventType]handlers.Handler)
 
-	followers := handlers.NewFollowerNotificationHandler(targets, userBackend)
+	followers := handlers.NewFollowerNotificationHandler(settings, userBackend)
 	notificationHandlers[followers.Type()] = followers
 
-	creation := handlers.NewRoomCreationNotificationHandler(targets, userBackend)
+	creation := handlers.NewRoomCreationNotificationHandler(settings, userBackend)
 	notificationHandlers[creation.Type()] = creation
 
-	invite := handlers.NewRoomInviteNotificationHandler(targets, userBackend)
+	invite := handlers.NewRoomInviteNotificationHandler(settings, userBackend)
 	notificationHandlers[invite.Type()] = invite
 
 	conn, err := grpc.Dial(fmt.Sprintf("%s:%d", roomsAddr.Host, roomsAddr.Port), grpc.WithInsecure())
@@ -142,7 +165,7 @@ func setupHandlers(db *sqldb.DB, roomsAddr conf.AddrConf) map[pubsub.EventType]h
 		log.Fatal(err)
 	}
 
-	join := handlers.NewRoomJoinNotificationHandler(targets, pb.NewRoomServiceClient(conn))
+	join := handlers.NewRoomJoinNotificationHandler(settings, roompb.NewRoomServiceClient(conn))
 	notificationHandlers[join.Type()] = join
 
 	welcome := handlers.NewWelcomeRoomNotificationHandler(userBackend)

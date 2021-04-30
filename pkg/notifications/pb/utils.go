e33032cf8fab6f579b0e0ce674093b7a543a0f6e
decanus <7621705+decanus@users.noreply.github.com>
2021-04-30 15:55:45 +0200

    fix

--- pkg/notifications/pb/utils.go
+++ pkg/notifications/pb/utils.go
@@ -6,11 +6,21 @@ func (x *Notification) ToPushNotification() *notifications.PushNotification {
 	res := &notifications.PushNotification{
 		Category:   notifications.NotificationCategory(x.Category),
 		CollapseID: x.CollapseId,
+		Arguments:  make(map[string]interface{}),
 		Alert: notifications.Alert{
 			Body:      x.Alert.Body,
 			Key:       x.Alert.LocalizationKey,
 			Arguments: x.Alert.LocalizationArguments,
 		},
+	}
+
+	for key, arg := range x.Arguments {
+		switch arg.Value.(type) {
+		case *Notification_Argument_Int:
+			res.Arguments[key] = arg.GetInt()
+		case *Notification_Argument_Str:
+			res.Arguments[key] = arg.GetStr()
+		}
 	}
 
 	return res
